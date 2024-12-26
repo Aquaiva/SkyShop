d5eb2debf29559aa52954be80993d8c0df5fef72
Valery Dub <cosmanatom@gmail.com>
2024-12-26 16:49:27 +0300

    commit1

---
+++ src/org/skypro/skyshop/App.java
@@ -0,0 +1,33 @@
+package org.skypro.skyshop;
+
+import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.Product;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        ProductBasket basket = new ProductBasket("Корзина1", "Пользователь1");
+        Product product1 = new Product("Ноутбук", 1000);
+        Product product2 = new Product("Смартфон", 800);
+        Product product3 = new Product("HDMI кабель", 20);
+        Product product4 = new Product("Наушники", 200);
+        Product product5 = new Product("Звуковая карта", 300);
+        Product product6 = new Product("Аудиосистема", 1000);
+        basket.addProduct(product1);
+        basket.addProduct(product2);
+        basket.addProduct(product3);
+        basket.addProduct(product4);
+        basket.addProduct(product5);
+        basket.addProduct(product6);
+        basket.printCartContents();
+
+        System.out.println("Найти 'Смартфон':" + (basket.findProduct("Смартфон") ? "найден" : "не найден"));
+        System.out.println("Найти 'Аудиосистема':" + (basket.findProduct("Аудиосистема") ? "найден" : "не найден"));
+        basket.calculateTotalPrice();
+        basket.clearCart();
+        basket.calculateTotalPrice();
+        basket.printCartContents();
+        System.out.println("Найти 'Смартфон':" + (basket.findProduct("Смартфон") ? "найден" : "не найден"));
+
+    }
+}
---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,69 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductBasket {
+    private String cartName;
+    private String userName;
+    private List<Product> products;
+    private static final int MAX_PRODUCTS = 5;
+
+    public ProductBasket(String cartName, String userName) {
+        this.cartName = cartName;
+        this.userName = userName;
+        this.products = new ArrayList<>();
+    }
+    //метод удаления товара из корзины
+    public void removeProduct(Product product) {
+        if (products.remove(product)) {
+            System.out.println(product.getName() + " удален из корзиныю");
+        } else {
+            System.out.println(product.getName() + " не найден в корзине.");
+        }
+    }
+    //метод добавления товара в корзину
+    public void addProduct(Product product) {
+        if (products.size()<MAX_PRODUCTS) {
+            products.add(product);
+            System.out.println(product.getName() + " добавлен в корзину.");
+        } else {
+            System.out.println("Невозможно добавить " + product.getName() + ": корзина заполнена (максимум " + MAX_PRODUCTS + " товаров).");
+        }
+    }
+    //метод для подсчета стоимости корзины
+    public double calculateTotalPrice() {
+        double total = 0.0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+    // метод поиска товара в корзине
+    public boolean findProduct(String productName) {
+        for (Product product : products) {
+            if (product.getName().equalsIgnoreCase(productName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //метод печати содержимого корзины
+    public void printCartContents() {
+        System.out.println("Содержимое корзины " + cartName + " пользователя " + userName + ":");
+        if (products.isEmpty()) {
+            System.out.println("Корзина пуста.");
+        } else { for (Product product : products) {
+            System.out.println(product);
+        }
+            System.out.printf("Общая стоимость: %.2f\n", calculateTotalPrice() );
+        }
+    }
+    ///метод очистки корзины
+    public void clearCart() {
+        products.clear();
+        System.out.println("Корзина очищена.");
+    }
+}
